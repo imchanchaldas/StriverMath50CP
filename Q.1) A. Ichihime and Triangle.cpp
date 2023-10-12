6796c6f3f1ba6a9e2c53b4f4caa734fc53275725
imchanchaldas <60842195+imchanchaldas@users.noreply.github.com>
2023-10-13 00:02:50 +0530

    Create Q.1) A. Ichihime and Triangle.cpp

---
+++ Q.1) A. Ichihime and Triangle.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	
+	int q;
+	cin>>q;
+	while(q--){
+		long long int a,b,c,d;
+		cin>>a>>b>>c>>d;
+		cout<<b<<" "<< c<<" "<<c<<endl;
+	}
+
+
+
+
+	return 0;
+}
+
+
+
+
+
+
