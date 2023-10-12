e074b127147ef8f77887f746f8b413e7a1780366
imchanchaldas <60842195+imchanchaldas@users.noreply.github.com>
2023-10-13 00:04:08 +0530

    Create Q.3) A. Mahmoud and Ehab and the even-odd game.cpp

---
+++ Q.3) A. Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define INF (1 << 29)
+// #define ll long long
+// const int N = 1e5 + 5;
+ 
+// void solve() {
+//   int l, r;
+//   cin >> l >> r;
+//   if (1LL * 2 * l > 1LL * r) {
+//     cout << -1 << ' ' << -1 << '\n';
+//   } else {
+//     cout << l << ' ' << 2 * l << '\n';
+//   }
+// }
+ 
+int main() {
+
+
+	#ifndef ONLINE_JUDGE
+	 freopen("input.txt","r",stdin);
+	 freopen("output1.txt","w",stdout);
+	 #endif
+
+	 ios::sync_with_stdio(false);
+	 cin.tie(nullptr);
+	 cout.tie(nullptr);
+	 long long int num;
+	 cin>>num;
+	 
+	 	if(num%2==0) cout<<"Mahmoud"<<endl;
+	 	else cout<<"Ehab"<<endl;
+	 
+
+	 
+	  return 0;
+}
+
+
+
+
+
+ 
